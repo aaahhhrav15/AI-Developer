abac661f3e2ac92c1b9f711a9cad85d55b3d7c76
aaahhhrav15 <lodhaaarav12005@gmail.com>
2025-02-26 15:49:34 +0530

    solved the isssue with the get all users route

--- src/app/api/users/all/route.js
+++ src/app/api/users/all/route.js
@@ -1,19 +1,29 @@
 import { NextResponse } from 'next/server';
-import userModel from '@/models/user.model';
-import userService from '@/services/user.service';
-import { authMiddleware } from '@/middleware/auth.middleware';
+import userModel from '@/models/userModel';
+import { getAllUsers } from '@/services/userService';
+import { authMiddleware } from '@/middleware/authMiddleware';
+import connectDB from '@/lib/db';
 
-export async function GET(req) {
+export async function GET(req) 
+{
+  await connectDB();
   const user = await authMiddleware(req);
-  if (!user) return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
 
-  try {
+  if (!user) return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+  
+  try 
+  {
+
     const loggedInUser = await userModel.findOne({ email: user.email });
-    const allUsers = await userService.getAllUsers({ userId: loggedInUser._id });
+    if (!loggedInUser) return NextResponse.json({ error: 'User not found' }, { status: 404 });
+
+    const allUsers = await getAllUsers({ userId: loggedInUser._id });
 
     return NextResponse.json({ users: allUsers });
 
-  } catch (error) {
+  } 
+  catch (error) 
+  {
     return NextResponse.json({ error: error.message }, { status: 400 });
   }
 }
